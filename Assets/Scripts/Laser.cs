7b47a49748dfbd3a8d4479015949e49097aee0be
Ina Joe <ij2049@nyu.edu>
2021-08-10 12:27:43 -0400

    alarm scene is added! - laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lr;
+    
+    [SerializeField] 
+    private Transform startPoint;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, startPoint.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+
+            if (hit.transform.tag == "Player")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        else
+        {
+            //don't go the laser forever. When this hit -transform.right * 5000 stop it
+            lr.SetPosition(1, transform.up * 5000);
+        }
+    }
+}
