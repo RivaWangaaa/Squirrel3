c3729c42ad9e00c08fb45f3db6b21ded0dc94e42
Yanxi <w1y2f3@126.com>
2022-06-27 17:12:40 -0400

    Implemented illustration mechanism to basement level

---
+++ Assets/Scripts/Illustration Scripts/Item.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Item : MonoBehaviour
+{
+    public GameObject illustrationCanvas;
+
+    public GameObject illustrationCamera;
+
+    public GameObject player;
+
+
+    //To show the cursor when open up UI
+    public void EnableFPS(bool enable)
+    {
+        FirstPersonDrifter fpsScript = player.GetComponent<FirstPersonDrifter>();
+
+        if (enable)
+        {
+            //Enable FPS script
+            fpsScript.enabled = true;
+        }
+        else
+        {
+            //Disable FPS script
+            fpsScript.enabled = false;
+            //Unlock Mouse and make it visible
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Item triggered");
+            
+            illustrationCanvas.SetActive(true);
+            player.SetActive(false);
+            illustrationCamera.SetActive(true);
+            EnableFPS(false);
+        }
+    }
+    
+    //Click on Back button, back to game
+    public void Back()
+    {
+        Debug.Log("Back to game");
+        player.transform.position += Vector3.forward;
+        EnableFPS(true);
+        illustrationCanvas.SetActive(false);
+        player.SetActive(true);
+        illustrationCamera.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Illustration Scripts/Nut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nut : MonoBehaviour
+{
+    private UIManager UIManagerInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UIManager.NutNum += 1;
+            Debug.Log("Nut collected." + "Nuts: " + UIManager.NutNum);
+            this.gameObject.SetActive(false);
+        }
+    }
+    
+}
